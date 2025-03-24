832cf1696ff7c119b74d1b05a05d261a94ccdce7
Jose-Marcosds <jcjmds@gmail.com>
2025-03-24 09:21:43 -0300

    Primeiras configuraçoes em css e javascript

---
+++ pagina01/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const pedidosPendentes = 12;
+    const maquinasAtivas = 5;
+    const operadores = 8;
+
+    document.getElementById("pedidosPendentes").textContent = pedidosPendentes;
+    document.getElementById("maquinasAtivas").textContent = maquinasAtivas;
+    document.getElementById("operadores").textContent = operadores;
+});
